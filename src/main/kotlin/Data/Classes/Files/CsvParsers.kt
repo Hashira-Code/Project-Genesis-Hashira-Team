23bd9db216caba76eb9b721915769d7d4fea0192
Alaa Mousa <https://github.com/alaamonir02/Basic_calculator.git>
2025-11-21 11:16:31 +0200

    Add fun parsePerformanceData()

--- src/main/kotlin/Data/Classes/Files/CsvParsers.kt
+++ src/main/kotlin/Data/Classes/Files/CsvParsers.kt
@@ -1,11 +1,19 @@
 package Data.Classes.Files
 import java.io.File
 fun parseMenteData(): List<MenteeRaw> {
-    val lines = File("src/main/resources/mentees.csv").readLines()
-    return lines.drop(1).map{line->
+    val lines1 = File("src/main/resources/mentees.csv").readLines()
+    return lines1.drop(1).map{line->
         val parts = line.split(",")
         MenteeRaw(parts[0].trim(),parts[1].trim(),parts[2].trim())
     }
 }
+fun parsePreformanceData(): List<PerformanceRaw>{
+    val lines2 = File("src/main/resources/performance.csv").readLines()
+    return lines2.drop(1).map { line2->
+        val parts2 = line2.split(",")
+        PerformanceRaw(parts2[0].trim(),parts2[1].trim(),parts2[2].trim(),parts2[3].trim())
+    }
 
+
+}
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,13 @@
 import Data.Classes.Files.parseMenteData
+import Data.Classes.Files.parsePreformanceData
+
 fun main(){
     val mentee = parseMenteData()
     println(mentee)
-}
 
+
+    val performance = parsePreformanceData()
+    println(performance)
+}
 
 
