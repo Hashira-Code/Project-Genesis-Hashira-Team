e1286413637535ade3b90f248a0c8da6a770fcb9
Doaa Allaham <yahyadoaa25@gmail.com>
2025-11-23 01:10:11 +0200

    Update Data Classes Files

--- src/main/kotlin/Data/Classes/Files/CsvParsers.kt
+++ src/main/kotlin/Data/Classes/Files/CsvParsers.kt
@@ -1,5 +1,27 @@
 package Data.Classes.Files
 import java.io.File
+fun parseMenteData(): List<MenteeRaw> {
+    val lines1 = File("src/main/resources/mentees.csv").readLines()
+    return lines1.drop(1).map{line->
+        val parts = line.split(",")
+        MenteeRaw(parts[0].trim(),parts[1].trim(),parts[2].trim())
+    }
+}
+fun parsePreformanceData(): List<PerformanceRaw>{
+    val lines2 = File("src/main/resources/performance.csv").readLines()
+    return lines2.drop(1).map { line2->
+        val parts2 = line2.split(",")
+        PerformanceRaw(parts2[0].trim(),parts2[1].trim(),parts2[2].trim(),parts2[3].trim())
+    }
 
+
+}
+fun parseTeamData(): List<TeamRaw> {
+    return File("src/main/resources/teams.csv")
+        .readLines()
+        .drop(1)
+        .map { it.split(",") }
+        .map { TeamRaw(it[0], it[1], it[2]) }
+}
 
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,19 @@
 
-fun main() {
-    println("ByteBloom Academy: Ecosystem Project Starter")
-    println("✅ Project setup is correct and runnable.")
-}
+import Data.Classes.Files.parseMenteData
+import Data.Classes.Files.parsePreformanceData
+import Data.Classes.Files.parseTeamData
 
+fun main(){
+    val mentee = parseMenteData()
+    println(mentee.size)
+
+
+    val performance = parsePreformanceData()
+    println(performance.size)
+
+    val teams = parseTeamData()
+    println(teams.size)
+
+}
 
 
