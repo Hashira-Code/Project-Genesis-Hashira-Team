2442f97603eae49b1451504f748ca0f796b8f1be
Doaa Allaham <yahyadoaa25@gmail.com>
2025-12-02 18:13:05 +0200

    Update cashed function ..

--- src/main/kotlin/CsvParsers.kt
+++ src/main/kotlin/CsvParsers.kt
@@ -2,45 +2,63 @@ package models
 
 import java.io.File
 
+
+private var cachedMentees: List<MenteeRaw>? = null
+
 fun parseMenteeData(): List<MenteeRaw> {
-    return File("src/main/resources/mentees.csv")
-        .readLines()
-        .drop(1)
-        .map { line ->
-            val fields = line.split(",")
-            MenteeRaw(
-                fields[0].trim(),
-                fields[1].trim(),
-                fields[2].trim()
-            )
-        }
+    if (cachedMentees == null) {
+        cachedMentees = File("src/main/resources/mentees.csv")
+            .readLines()
+            .drop(1)
+            .map { line ->
+                val fields = line.split(",")
+                MenteeRaw(
+                    fields[0].trim(),
+                    fields[1].trim(),
+                    fields[2].trim()
+                )
+            }
+    }
+    return cachedMentees!!
 }
+
+
+private var cachedPerformances: List<PerformanceRaw>? = null
 
 fun parsePerformanceData(): List<PerformanceRaw> {
-    return File("src/main/resources/performance.csv")
-        .readLines()
-        .drop(1)
-        .map { line ->
-            val fields = line.split(",")
-            PerformanceRaw(
-                fields[0].trim(),
-                fields[1].trim(),
-                fields[2].trim(),
-                fields[3].trim()
-            )
-        }
+    if (cachedPerformances == null) {
+        cachedPerformances = File("src/main/resources/performance.csv")
+            .readLines()
+            .drop(1)
+            .map { line ->
+                val fields = line.split(",")
+                PerformanceRaw(
+                    fields[0].trim(),
+                    fields[1].trim(),
+                    fields[2].trim(),
+                    fields[3].trim()
+                )
+            }
+    }
+    return cachedPerformances!!
 }
 
+
+private var cachedTeams: List<TeamRaw>? = null
+
 fun parseTeamData(): List<TeamRaw> {
-    return File("src/main/resources/teams.csv")
-        .readLines()
-        .drop(1)
-        .map { line ->
-            val fields = line.split(",")
-            TeamRaw(
-                fields[0].trim(),
-                fields[1].trim(),
-                fields[2].trim()
-            )
-        }
+    if (cachedTeams == null) {
+        cachedTeams = File("src/main/resources/teams.csv")
+            .readLines()
+            .drop(1)
+            .map { line ->
+                val fields = line.split(",")
+                TeamRaw(
+                    fields[0].trim(),
+                    fields[1].trim(),
+                    fields[2].trim()
+                )
+            }
+    }
+    return cachedTeams!!
 }
