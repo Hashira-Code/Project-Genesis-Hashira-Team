904a36f0a92eeb98fdcf97f51d4c31b57d644587
Doaa Allaham <yahyadoaa25@gmail.com>
2025-12-03 01:44:22 +0200

    Setup domain package and empty classes for Week 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,6 @@
 
-import models.parseMenteeData
-import models.parsePerformanceData
-import models.parseTeamData
 
 fun main(){
-    val mentee = parseMenteeData()
-    println(mentee.size)
-
-    val performance = parsePerformanceData()
-    println(performance.size)
-
-    val teams = parseTeamData()
-    println(teams.size)
 
 
 }
