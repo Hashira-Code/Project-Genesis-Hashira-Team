7a1e44674afcb6a1666ab99a97e4b4ef650038eb
tasneemmazen85-design <tasneemmazen85@gmail.com>
2025-12-05 15:37:51 +0300

    create buildSubmissions  and buildDomain functions and PerformanceSubmission data class

--- src/main/kotlin/domain/DomainBuilder.kt
+++ src/main/kotlin/domain/DomainBuilder.kt
@@ -31,6 +31,7 @@ class DomainBuilder {
                     teamId = rawMentee.teamId
                 )
                 mentee.team = team
+                team?.mentees?.add(mentee)
                 mentee
             }
 
@@ -39,22 +40,27 @@ class DomainBuilder {
 
 
 
+    fun buildSubmissions(rawSubmissions: List<PerformanceRaw>, mentees: List<Mentee>) {
+        val menteesById = mentees.associateBy { it.menteeId }
+        rawSubmissions.forEach { raw ->
+            val submission = PerformanceSubmission(
+                menteeId = raw.menteeId,
+                submissionId = raw.submissionId,
+                submissionType = raw.submissionType,
+                score = raw.score
+            )
+            menteesById[raw.menteeId]?.submissions?.add(submission)
+        }
+    }
+    fun buildDomain(
+        rawTeams: List<TeamRaw>,
+        rawMentees: List<MenteeRaw>,
+        rawSubmissions: List<PerformanceRaw>
+    ): List<Team> {
+        val teams = buildTeams(rawTeams)
+        val mentees = buildMentees(rawMentees, teams)
+        buildSubmissions(rawSubmissions, mentees)
+        return teams
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-fun buildSubmissions(rawSubmissions: List<PerformanceRaw>, mentees: List<Mentee>): List<PerformanceSubmission> {
-        return listOf()
-
-
-}
 }
