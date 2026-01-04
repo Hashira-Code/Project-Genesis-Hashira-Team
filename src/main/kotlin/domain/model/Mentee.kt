739d36ad837bc82581765e634f59929bd3a8ba88
Mona Abu Nada <monaabonada409755535@gmail.com>
2026-01-04 14:30:12 +0200

    Add classes Domain

---
+++ src/main/kotlin/domain/model/Mentee.kt
@@ -0,0 +1,7 @@
+package domain.model
+
+data class Mentee(
+    val id: String,
+    val name: String,
+    val teamId: String,
+    )
---
+++ src/main/kotlin/domain/model/Project.kt
@@ -0,0 +1,7 @@
+package domain.model
+
+data class Project(
+    val id: String,
+    val name: String,
+    val teamId: String
+)
