d0ee01bd515b14d5e6d62ddc6b24a2d8387c31da
Shahed <shahedmusallm@gmail.com>
2026-01-22 21:58:11 +0200

    Maper

--- src/main/kotlin/mapper/TeamMapper.kt
+++ src/main/kotlin/mapper/TeamMapper.kt
@@ -4,7 +4,7 @@ import dataRaw.teamRaw
 import domain.model.Team
 
 class TeamMapper: Mapper<teamRaw,Team> {
-    override fun toDaomain(rawList: List<teamRaw>): List<Team> {
+    override fun toDomain(rawList: List<teamRaw>): List<Team> {
         return rawList.map { Raw ->
             Team(
                 id = Raw.id,
