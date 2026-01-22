c66fb335f7b063e33486b47b65ec26b2bbe56090
Shahed <shahedmusallm@gmail.com>
2026-01-22 17:07:48 +0200

    Create-PerformanceRepositoryImplementation

--- src/main/kotlin/domain/repository/PerformanceRepository.kt
+++ src/main/kotlin/domain/repository/PerformanceRepository.kt
@@ -7,4 +7,5 @@ interface PerformanceRepository {
     fun getAll(): List<PerformanceSubmission>
     fun getByMenteeId(menteeId: String): List<PerformanceSubmission>
     fun getByType(type: SubmissionType): List<PerformanceSubmission>
+ //
 }
