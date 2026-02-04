bc967b5f3ace0aaada2dd28be04bb7373675ba43
Raghad Abbas <munirraghad1612@gmail.com>
2026-02-04 16:56:52 +0200

    Add team attendance report use case

--- src/main/kotlin/domain/usecase/TeamAttendanceReportUseCase.kt
+++ src/main/kotlin/domain/usecase/TeamAttendanceReportUseCase.kt
@@ -1,10 +1,24 @@
 package domain.usecase
-
+import domain.repository.MenteeRepo
 import domain.repository.TeamRepo
 
 class TeamAttendanceReportUseCase(
     private val teamRepo: TeamRepo,
+    private val menteeRepo: MenteeRepo,
     private val calculateAttendancePercentage: CalculateAttendancePercentageUseCase
 ) {
-  
-}
+    operator fun invoke(): Map<String, List<Pair<String, Double>>> {
+            val teams = teamRepo.getAll()
+            val mentees = menteeRepo.getAll()
+            val attendancePercentages = calculateAttendancePercentage()
+            return teams.associate { team ->
+                val teamMembers =
+                    mentees
+                        .filter { it.teamId == team.id }
+                        .map { mentee ->
+                            mentee.name to (attendancePercentages[mentee] ?: 0.0)
+                        }
+                team.name to teamMembers
+            }
+        }
+    }
