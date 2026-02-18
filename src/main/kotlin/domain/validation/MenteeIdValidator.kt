65f2b6de8c92cf3131bd5d0afbc45f932b45c4d1
Mona Abu Nada <monaabonada409755535@gmail.com>
2026-02-18 15:37:17 +0200

    Add MenteeIdValidator for mentee id validation

---
+++ src/main/kotlin/domain/validation/MenteeIdValidator.kt
@@ -0,0 +1,18 @@
+package domain.validation
+
+import domain.exception.IdValidationException
+
+class MenteeIdValidator : Validator<String> {
+    override fun validate(value: String): Result<String> {
+        if (value.isBlank()) {
+            return Result.failure(IdValidationException("Mentee ID Cannot be blank"))
+        }
+        if (!value.matches(Regex("^m\\d+$"))) {
+            return Result.failure(IdValidationException("Mentee ID must start wiht 'm' followed by digits"))
+
+        }
+        return Result.success(value)
+
+    }
+
+}
