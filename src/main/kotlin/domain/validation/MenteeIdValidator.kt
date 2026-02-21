8f2b903449dd33c077b33d672ce02876fece62d7
Mona Abu Nada <monaabonada409755535@gmail.com>
2026-02-21 15:04:21 +0200

    Refactor MenteeIdValidator  to support generic validtion contract

--- src/main/kotlin/domain/validation/MenteeIdValidator.kt
+++ src/main/kotlin/domain/validation/MenteeIdValidator.kt
@@ -2,7 +2,7 @@ package domain.validation
 
 import domain.exception.IdValidationException
 
-class MenteeIdValidator : Validator<String> {
+class MenteeIdValidator : Validator<String,String> {
     override fun validate(value: String): Result<String> {
         if (value.isBlank()) {
             return Result.failure(IdValidationException("Mentee ID Cannot be blank"))
