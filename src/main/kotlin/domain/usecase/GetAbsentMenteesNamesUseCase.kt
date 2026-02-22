b144d98694fa0cce6027f7a512e91a5813e75abd
Mona Abu Nada <monaabonada409755535@gmail.com>
2026-02-22 16:15:58 +0200

    Refactor GetAbsentMenteesNamesUseCase to validate input using weekNumberValidator and  return Result

--- src/main/kotlin/domain/usecase/GetAbsentMenteesNamesUseCase.kt
+++ src/main/kotlin/domain/usecase/GetAbsentMenteesNamesUseCase.kt
@@ -3,34 +3,39 @@ import domain.model.Attendance
 import domain.model.AttendanceStatus
 import domain.repository.MenteeRepo
 import domain.repository.AttendanceRepo
+import domain.request.WeekNumberRequest
+import domain.validation.Validator
 
 class GetAbsentMenteesNamesUseCase(
     private val attendanceRepo: AttendanceRepo,
-    private val menteeRepo: MenteeRepo
+    private val menteeRepo: MenteeRepo,
+    private val weekNumberValidator: Validator<String, Int>
 ) {
 
-    operator fun invoke(weekNumber: Int): List<String> {
+    operator fun invoke(request: WeekNumberRequest): Result<List<String>> {
+        return weekNumberValidator.validate(request.rawWeekNumber)
+            .fold(
+            onSuccess = { weekNumber ->
+                val absentIds = attendanceRepo
+                    .getByWeekNumber(weekNumber)
+                    .asSequence()
+                    .filter { it.status == AttendanceStatus.ABSENT }
+                    .map { it.menteeId }
+                    .toSet()
+                val names = menteeRepo
+                    .getAll()
+                    .asSequence()
+                    .filter { it.id in absentIds }
+                    .map { it.name }
+                    .toList()
+                Result.success(names)
 
-        require(weekNumber > MINIMUM_WEEK_NUMBER)
+            }
+            ,onFailure = { error -> Result.failure(error) }
 
-        val absentIds = attendanceRepo
-            .getByWeekNumber(weekNumber)
-            .asSequence()
-            .filter { it.status == AttendanceStatus.ABSENT }
-            .map { it.menteeId }
-            .toSet()
 
-        if (absentIds.isEmpty()) return emptyList()
-
-        return menteeRepo.getAll()
-            .asSequence()
-            .filter { it.id in absentIds }
-            .map { it.name }
-            .toList()
+        )
     }
-    companion object {
-        private const val MINIMUM_WEEK_NUMBER=100.0
 
-    }
 
 }
