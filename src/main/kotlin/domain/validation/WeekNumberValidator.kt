0369ad5a2d74b6c0141225f2123b34cfb3a44680
Mona Abu Nada <monaabonada409755535@gmail.com>
2026-02-22 14:52:55 +0200

    Refactor WeekNumberValidator to support generic validation and type conversion

--- src/main/kotlin/domain/validation/WeekNumberValidator.kt
+++ src/main/kotlin/domain/validation/WeekNumberValidator.kt
@@ -1,15 +1,26 @@
 package domain.validation
 
+import domain.exception.IdValidationException
 import domain.exception.WeekNumberValidationException
 
-class WeekNumberValidator : Validator<Int, Int> {
-    override fun validate(value: Int): Result<Int> {
-        return if (value != 0) {
-            Result.success(value)
-        } else {
-            Result.failure(
-                WeekNumberValidationException("Week number cannot be zero"))
+class WeekNumberValidator : Validator<String, Int> {
+    override fun validate(value: String): Result<Int> {
+        if (value.isBlank()) {
+            return Result.failure(
+                WeekNumberValidationException("Week number cannot be empty")
+            )
         }
+        val weekNumber = value.toIntOrNull() ?:
+        return Result.failure(
+            WeekNumberValidationException("Week number must be valid"))
+
+        if (weekNumber <= 0) {
+            return Result.failure(
+                WeekNumberValidationException("Week must be greater than zero"))
+
+        }
+        return Result.success(weekNumber)
     }
+
 }
 
