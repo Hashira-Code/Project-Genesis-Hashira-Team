4572f3a16d488438f5aa731deac39a3582475f29
Mona Abu Nada <monaabonada409755535@gmail.com>
2026-03-02 23:02:15 +0200

    implement custom exceptions in validators and clean up logic

--- src/main/kotlin/domain/model/exception/ValidationExeption.kt
+++ src/main/kotlin/domain/model/exception/ValidationExeption.kt
@@ -6,10 +6,12 @@ open class ValidationException(message: String) : Exception(message) {
     class ScoreValidationException(message: String) : ValidationException(message)
     class WeekNumberValidationException(message: String) : ValidationException(message)
 }
+
 class EmptyFieldException(message: String = "Field cannot be empty") : ValidationException(message)
 class InvalidFormatException(message: String = "Invalid format") : ValidationException(message)
 class ValueOutOfRangeException(message: String = "Value out of allowed range") : ValidationException(message)
 class EntityNotFoundException(message: String) : Exception(message)
-class MenteeIdEmptyException(message: String): Exception(message)
+class MenteeIdEmptyException(message: String) : Exception(message)
+class InvalidNameLengthException(message: String) : Exception(message)
 
 
--- src/main/kotlin/domain/validation/NameValidator.kt
+++ src/main/kotlin/domain/validation/NameValidator.kt
@@ -1,25 +1,27 @@
 package domain.validation
 
-import domain.model.exception.ValidationException.NameValidationException
+import domain.model.exception.EmptyFieldException
+import domain.model.exception.InvalidNameLengthException
 
 class NameValidator : Validator<String, String> {
     override fun validate(value: String): Result<String> {
 
-        return value.takeIf { it.isNotBlank() }
-            ?.takeIf { it.length >= MIN_LENGTH }
-            ?.let { Result.success(it) }
-            ?: Result.failure(
-                NameValidationException(
-                    when {
-                        value.isBlank() -> EMPTY_MSG
-                        else -> LENGTH_MSG
-                    }
-                )
+        if (value.isBlank()) {
+            return Result.failure(
+                EmptyFieldException(EMPTY_MSG)
             )
+        }
+
+        if (value.length < MIN_LENGTH) {
+            return Result.failure(
+                InvalidNameLengthException(LENGTH_MSG)
+            )
+        }
+        return Result.success(value)
     }
 
     companion object {
-        private const val MIN_LENGTH = 3
+        private const val MIN_LENGTH = 2
         private const val EMPTY_MSG = "Name cannot be blank"
         private const val LENGTH_MSG = "Name must be at least 3 characters"
     }
