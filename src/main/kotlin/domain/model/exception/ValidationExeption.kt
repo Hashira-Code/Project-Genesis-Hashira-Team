7f3fbe24b09385dd2dee8c0f3f92f3457a9cf81f
ALBatool8 <tot28820041@gmail.com>
2026-03-04 14:50:56 +0200

    update UseCase logic with new exceptions and companion object

--- src/main/kotlin/domain/model/exception/ValidationExeption.kt
+++ src/main/kotlin/domain/model/exception/ValidationExeption.kt
@@ -4,9 +4,10 @@ open class ValidationException(message: String) : Exception(message) {
     class EmptyFieldException(message: String) : ValidationException(message)
     class InvalidFormatException(message: String) : ValidationException(message)
     class ValueOutOfRangeException(message: String) : ValidationException(message)
-    class EntityNotFoundException(message: String) : Exception(message)
-    class MenteeIdEmptyException(message: String) : Exception(message)
-    class InvalidNameLengthException(message: String) : Exception(message)
+    class EntityNotFoundException(message: String) : ValidationException(message)
+    class MenteeIdEmptyException(message: String) : ValidationException(message)
+    class InvalidNameLengthException(message: String) : ValidationException(message)
+    class DataNotFoundException(message: String) : ValidationException(message)
 }
 
 
