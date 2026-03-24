9f0660b9c759bf096fc70748db29f5756979193c
Mona Nada_hub <monaabonada409755535@gmail.com>
2026-03-25 00:11:33 +0200

    bind data and domain validators in ValidatorModule

---
+++ src/main/kotlin/di/ValidatorModule.kt
@@ -0,0 +1,18 @@
+package di
+
+import data.validation.EmptyFieldValidator
+import data.validation.FileValidator
+import data.validation.LineIsNotEmptyValidator
+import data.validation.Validator
+import domain.validation.WeekNumberValidator
+import org.koin.dsl.module
+import java.io.File
+import domain.validation.Validator as DomainValidator
+
+
+    val validatorModule = module {
+        single<Validator<File>> { FileValidator() }
+        single<Validator<String>> { LineIsNotEmptyValidator() }
+        single<Validator<List<String>>> { EmptyFieldValidator() }
+        single<DomainValidator<Int, Int>> { WeekNumberValidator() }
+    }
