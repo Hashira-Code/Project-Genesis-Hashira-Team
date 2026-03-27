d8b56ca3c46c762f720517f022aa9b292c44ea21
Mona Nada_hub <monaabonada409755535@gmail.com>
2026-03-27 23:30:43 +0200

    add green success case for FindTeamWorkingOnProjectUseCase

--- src/main/kotlin/domain/usecase/FindTeamWorkingOnProjectUseCase.kt
+++ src/main/kotlin/domain/usecase/FindTeamWorkingOnProjectUseCase.kt
@@ -1,18 +1,25 @@
 package domain.usecase
 
-import domain.model.entity.Project
 import domain.model.entity.Team
 import domain.model.request.ProjectIdRequest
 import domain.repository.ProjectRepo
 import domain.repository.TeamRepo
-import domain.model.exception.ValidationException.DataNotFoundException
 
 class FindTeamWorkingOnProjectUseCase(
     private val projectRepo: ProjectRepo,
     private val teamRepo: TeamRepo
 ) {
     operator fun invoke(request: ProjectIdRequest): Result<Team> {
+        val project = projectRepo.getAll().getOrElse {
+            return Result.failure(it)
+        }.find { it.id == request.id }
+            ?: TODO("Handle missing project after writing the next failing test")
 
-        return TODO("Provide the return value")
+        val team = teamRepo.getById(project.teamId).getOrElse {
+            return Result.failure(it)
+        } ?: TODO("Handle missing team after writing the next failing test")
+
+        return Result.success(team)
     }
 }
+
