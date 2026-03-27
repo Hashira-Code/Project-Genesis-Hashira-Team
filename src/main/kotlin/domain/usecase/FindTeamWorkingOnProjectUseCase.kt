548f5e9283bdbc532f7e58d08a28824dae226d78
Mona Nada_hub <monaabonada409755535@gmail.com>
2026-03-27 23:42:34 +0200

    improve success flow in FindTeamWorkingOnProjectUseCase

--- src/main/kotlin/domain/usecase/FindTeamWorkingOnProjectUseCase.kt
+++ src/main/kotlin/domain/usecase/FindTeamWorkingOnProjectUseCase.kt
@@ -10,9 +10,11 @@ class FindTeamWorkingOnProjectUseCase(
     private val teamRepo: TeamRepo
 ) {
     operator fun invoke(request: ProjectIdRequest): Result<Team> {
-        val project = projectRepo.getAll().getOrElse {
-            return Result.failure(it)
-        }.find { it.id == request.id }
+        val projects = projectRepo.getAll().getOrElse {
+           return Result.failure(it)
+        }
+
+        val project = projects.find { it.id == request.id }
             ?: TODO("Handle missing project after writing the next failing test")
 
         val team = teamRepo.getById(project.teamId).getOrElse {
