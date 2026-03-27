b332bfb7c8b3bc01dbcc2d07e2683dd16480f7ae
ALBatool8 <tot28820041@gmail.com>
2026-03-27 18:30:14 +0200

    test: add FakePerformanceRepo for unit testing

---
+++ src/test/kotlin/testsupport/fake/repo/FakePerformanceRepo.kt
@@ -0,0 +1,17 @@
+package testsupport.fake.repo
+
+import domain.model.entity.PerformanceSubmission
+import domain.model.entity.SubmissionType
+import domain.repository.PerformanceRepo
+
+class FakePerformanceRepo(
+    private val Submissions: List<PerformanceSubmission> = emptyList()
+) : PerformanceRepo {
+    override fun getAll(): Result<List<PerformanceSubmission>> = Result.success(Submissions)
+
+    override fun getByMenteeId(menteeId: String): Result<List<PerformanceSubmission>> =
+        Result.success(Submissions.filter { it.menteeId == menteeId })
+
+    override fun getByType(type: SubmissionType): Result<List<PerformanceSubmission>> =
+        Result.success(Submissions.filter { it.type == type })
+}
