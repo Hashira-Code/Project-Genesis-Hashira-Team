fbfd0d69fe48f3fd93978d42fbf5ec1617a1f3f0
Mona Nada_hub <monaabonada409755535@gmail.com>
2026-03-31 15:23:36 +0300

    qualify mappers and inject explicit mapper instances in RepoModule

--- src/main/kotlin/di/RepoModule.kt
+++ src/main/kotlin/di/RepoModule.kt
@@ -2,12 +2,13 @@ package di
 
 import data.repository.*
 import domain.repository.*
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val repoModule=module {
-    single<MenteeRepo> { MenteeRepoImpl(get(),get())}
-    single<TeamRepo> { TeamRepoImpl(get(),get())}
-    single<ProjectRepo> { ProjectRepoImpl(get(),get())}
-    single<AttendanceRepo> { AttendanceRepoImpl(get(),get())}
-    single<PerformanceRepo> { PerformanceRepoImpl(get(),get())}
+    single<MenteeRepo> { MenteeRepoImpl(get(), get(named("menteeMapper"))) }
+    single<TeamRepo> { TeamRepoImpl(get(), get(named("teamMapper"))) }
+    single<ProjectRepo> { ProjectRepoImpl(get(), get(named("projectMapper"))) }
+    single<AttendanceRepo> { AttendanceRepoImpl(get(), get(named("attendanceMapper"))) }
+    single<PerformanceRepo> { PerformanceRepoImpl(get(), get(named("performanceMapper"))) }
 }
