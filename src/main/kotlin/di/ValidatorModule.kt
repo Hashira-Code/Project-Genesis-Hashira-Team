bd295a3de2f91e53a7655b35bf9909feb6497767
Mona Nada_hub <monaabonada409755535@gmail.com>
2026-03-31 14:39:25 +0300

    fix(di): qualify validators in ValidatorModule to prevent Koin generic injection mismatch

--- src/main/kotlin/di/ValidatorModule.kt
+++ src/main/kotlin/di/ValidatorModule.kt
@@ -5,13 +5,14 @@ import data.validation.FileValidator
 import data.validation.LineIsNotEmptyValidator
 import data.validation.Validator
 import domain.validation.WeekNumberValidator
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import java.io.File
 import domain.validation.Validator as DomainValidator
 
 val validatorModule = module {
-    single<Validator<File>> { FileValidator() }
-    single<Validator<String>> { LineIsNotEmptyValidator() }
-    single<Validator<List<String>>> { EmptyFieldValidator() }
+    single<Validator<File>>(named("fileValidator")) { FileValidator() }
+    single<Validator<String>>(named("lineValidator")) { LineIsNotEmptyValidator() }
+    single<Validator<List<String>>>(named("emptyFieldValidator")) { EmptyFieldValidator() }
     single<DomainValidator<Int, Int>> { WeekNumberValidator() }
 }
