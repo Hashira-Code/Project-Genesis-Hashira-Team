8d92ab7e240aef0ebe9b0a6802f1505947753565
Mona Nada_hub <monaabonada409755535@gmail.com>
2026-04-02 23:09:44 +0300

    add base koin test

---
+++ src/test/kotlin/support/BaseKoinTest.kt
@@ -0,0 +1,27 @@
+package support
+
+import org.junit.jupiter.api.AfterEach
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.core.module.Module
+
+abstract class BaseKoinTest : KoinComponent {
+
+    protected fun startKoinWith(
+        vararg modules: Module
+    ) {
+        stopKoin()
+        startKoin {
+            modules(modules.toList())
+        }
+    }
+
+    protected inline fun <reified T> resolve(): T = get()
+
+    @AfterEach
+    fun tearDownKoin() {
+        stopKoin()
+    }
+}
