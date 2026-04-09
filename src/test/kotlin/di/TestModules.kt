096a21377a0b1a67d44b4b4c250c125a269118e9
ALBatool8 <tot28820041@gmail.com>
2026-04-09 12:02:12 +0200

    refactor: simplify defaultTestModules by converting it to a static list of modules

--- src/test/kotlin/di/TestModules.kt
+++ src/test/kotlin/di/TestModules.kt
@@ -1,27 +1,7 @@
 package di
 
-import domain.model.entity.Attendance
-import domain.model.entity.Mentee
-import domain.model.entity.PerformanceSubmission
-import domain.model.entity.Project
-import domain.model.entity.Team
-import org.koin.core.module.Module
-import data.fixture.TestDataFactory
-
-fun defaultTestModules(
-    teams: List<Team> = TestDataFactory.defaultTeams(),
-    projects: List<Project> = TestDataFactory.defaultProjects(),
-    attendances: List<Attendance> = TestDataFactory.defaultAttendances(),
-    performances: List<PerformanceSubmission> = TestDataFactory.defaultPerformanceSubmissions(),
-    mentees: List<Mentee> = TestDataFactory.defaultMentees()
-): Array<Module> = arrayOf(
-    repoTestModule(
-        teams = teams,
-        projects = projects,
-        attendances = attendances,
-        performances = performances,
-        mentees = mentees
-    ),
-    validatorTestModule(),
-    useCaseTestModule()
+val defaultTestModules = listOf(
+    repoTestModule,
+    validatorTestModule,
+    useCaseTestModule
 )
