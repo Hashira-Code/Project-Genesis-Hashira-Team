150a6d7d08231b94f7a205d1103ab3ba7a2d7185
ALBatool8 <tot28820041@gmail.com>
2026-04-09 12:00:00 +0200

    refactor: convert repoTestModule to a static val using TestDataFactory

--- src/test/kotlin/di/RepoTestModule.kt
+++ src/test/kotlin/di/RepoTestModule.kt
@@ -1,10 +1,5 @@
 package di
 
-import domain.model.entity.Attendance
-import domain.model.entity.Mentee
-import domain.model.entity.PerformanceSubmission
-import domain.model.entity.Project
-import domain.model.entity.Team
 import domain.repository.AttendanceRepo
 import domain.repository.MenteeRepo
 import domain.repository.PerformanceRepo
@@ -19,16 +14,10 @@ import data.fake.FakeProjectRepo
 import data.fake.FakeTeamRepo
 import data.fixture.TestDataFactory
 
-fun repoTestModule(
-    teams: List<Team> = TestDataFactory.defaultTeams(),
-    projects: List<Project> = TestDataFactory.defaultProjects(),
-    attendances: List<Attendance> = TestDataFactory.defaultAttendances(),
-    performances: List<PerformanceSubmission> = TestDataFactory.defaultPerformanceSubmissions(),
-    mentees: List<Mentee> = TestDataFactory.defaultMentees()
-): Module = module {
-    single<TeamRepo> { FakeTeamRepo(teams) }
-    single<ProjectRepo> { FakeProjectRepo(projects) }
-    single<AttendanceRepo> { FakeAttendanceRepo(attendances) }
-    single<PerformanceRepo> { FakePerformanceRepo(performances) }
-    single<MenteeRepo> { FakeMenteeRepo(mentees) }
+val repoTestModule: Module = module {
+    single<TeamRepo> { FakeTeamRepo(TestDataFactory.defaultTeams()) }
+    single<ProjectRepo> { FakeProjectRepo(TestDataFactory.defaultProjects()) }
+    single<AttendanceRepo> { FakeAttendanceRepo(TestDataFactory.defaultAttendances()) }
+    single<PerformanceRepo> { FakePerformanceRepo(TestDataFactory.defaultPerformanceSubmissions()) }
+    single<MenteeRepo> { FakeMenteeRepo(TestDataFactory.defaultMentees()) }
 }
