b6e95bdcd9780b8d5cf743120fd0841f6ca28e9c
Mona Nada_hub <monaabonada409755535@gmail.com>
2026-04-11 13:07:15 +0300

    improve modularization and structure

--- src/test/kotlin/di/UseCaseTestModule.kt
+++ src/test/kotlin/di/UseCaseTestModule.kt
@@ -1,13 +1,19 @@
 package di
 
 import domain.usecase.EvaluateTeamHealthUseCase
+import domain.usecase.FindTeamWorkingOnProjectUseCase
 import domain.usecase.GetAbsentMenteesNamesUseCase
 import domain.usecase.GetAverageScorePerMenteeUseCase
+import domain.usecase.GetOverallPerformanceAverageForTeamUseCase
+import domain.usecase.GetTopPerformingMenteesBySubmissionTypeUseCase
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val useCaseTestModule: Module = module {
-    factory { GetAbsentMenteesNamesUseCase(get(), get(), get()) }
-    factory { GetAverageScorePerMenteeUseCase(get(), get()) }
-    factory { EvaluateTeamHealthUseCase(get(), get(), get(), get()) }
+    single { FindTeamWorkingOnProjectUseCase(get(), get()) }
+    single { GetAbsentMenteesNamesUseCase(get(), get(), get()) }
+    single { GetAverageScorePerMenteeUseCase(get(), get()) }
+    single { GetOverallPerformanceAverageForTeamUseCase(get(), get()) }
+    single { GetTopPerformingMenteesBySubmissionTypeUseCase(get(), get()) }
+    single { EvaluateTeamHealthUseCase(get(), get(), get(), get()) }
 }
