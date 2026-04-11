b57a0faec5e216b91be7d6d1b6040982cbd95809
Mona Nada_hub <monaabonada409755535@gmail.com>
2026-04-11 12:58:11 +0300

    test: simplify base koin setup

--- src/test/kotlin/data/BaseKoinTest.kt
+++ src/test/kotlin/data/BaseKoinTest.kt
@@ -1,25 +1,22 @@
 package data
 
+import data.fixture.TestDataFactory
+import di.defaultTestModules
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.get
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
-import org.koin.core.module.Module
 
 abstract class BaseKoinTest : KoinComponent {
 
-    protected fun startKoinWith(
-        vararg modules: Module
-    ) {
-        stopKoin()
+    @BeforeEach
+    fun setUpKoin() {
+        TestDataFactory.reset()
         startKoin {
-            modules(modules.toList())
+            modules(defaultTestModules)
         }
     }
-
-
-    protected inline fun <reified T> resolve(): T = get()
 
     @AfterEach
     fun tearDownKoin() {
