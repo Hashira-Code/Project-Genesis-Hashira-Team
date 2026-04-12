cc7f1b0e5054a5158fe226bfef4e4a1b7e5e4abc
Mona Nada_hub <monaabonada409755535@gmail.com>
2026-04-12 22:21:35 +0300

    Add FindTeamWorkingOnProjectUseCase to UseCaseTestModule

--- src/test/kotlin/di/UseCaseTestModule.kt
+++ src/test/kotlin/di/UseCaseTestModule.kt
@@ -1,10 +1,12 @@
 package di
 
 import domain.usecase.EvaluateTeamHealthUseCase
+import domain.usecase.FindTeamWorkingOnProjectUseCase
 import domain.usecase.GetAbsentMenteesNamesUseCase
 import domain.usecase.GetAverageScorePerMenteeUseCase
 import domain.usecase.GetOverallPerformanceAverageForTeamUseCase
 import org.koin.core.module.Module
+import org.koin.core.scope.get
 import org.koin.dsl.module
 
 val useCaseTestModule: Module = module {
@@ -13,4 +15,5 @@ val useCaseTestModule: Module = module {
     factory { EvaluateTeamHealthUseCase(get(), get(), get(), get()) }
     factory { GetAbsentMenteesNamesUseCase(get(), get(),get()) }
     factory { GetOverallPerformanceAverageForTeamUseCase(get(), get()) }
+    factory { FindTeamWorkingOnProjectUseCase(get(),get()) }
 }
