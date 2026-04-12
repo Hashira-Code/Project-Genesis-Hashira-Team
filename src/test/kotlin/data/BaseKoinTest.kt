7e896d6c88b2d6b3d8540dfa4d13b2242aaa7ccb
Mona Nada_hub <monaabonada409755535@gmail.com>
2026-04-12 17:24:09 +0300

    Refactor BaseKoinTest to simplify setup

--- src/test/kotlin/data/BaseKoinTest.kt
+++ src/test/kotlin/data/BaseKoinTest.kt
@@ -1,34 +1,21 @@
 package data
 
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
- development
-import org.koin.core.component.get
- main
 import org.koin.core.module.Module
 
 abstract class BaseKoinTest : KoinComponent {
 
- development
-
     @BeforeEach
     abstract fun setup()
 
-    protected fun startKoinWith(modules: Module){
+    protected fun startKoinWith(tsetModules: Module) {
         stopKoin()
-        startKoin { modules(modules) }
-
-    protected fun startKoinWith(
-        vararg modules: Module
-    ) {
-        stopKoin()
-        startKoin {
-            modules(modules.toList())
-        }
- main
+        startKoin { modules(tsetModules) }
     }
 
     protected inline fun <reified T> resolve(): T = get()
