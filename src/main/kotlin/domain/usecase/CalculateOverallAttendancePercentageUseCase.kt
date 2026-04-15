1b2460e70abe4a6a50956f436aafd3549051f506
Shahed-Hassan <2004shahed.hassan@gmail.com>
2026-04-15 19:24:17 +0300

    CalculateOverallAttendancePercentageUseCaseTest-green

--- src/main/kotlin/domain/usecase/CalculateOverallAttendancePercentageUseCase.kt
+++ src/main/kotlin/domain/usecase/CalculateOverallAttendancePercentageUseCase.kt
@@ -8,7 +8,25 @@ import domain.model.entity.AttendanceStatus
 class CalculateOverallAttendancePercentageUseCase(private val attendanceRepo: AttendanceRepo){
 
     operator fun invoke(menteeId: MenteeIdRequest):Result<Double>{
-       TODO()
+        val attendances: List<Attendance> = attendanceRepo.getByMenteeId(menteeId.id).getOrElse {
+            return Result.failure(it)
+        }
+
+        val points = attendances.sumOf { attendance ->
+            when (attendance.status) {
+                AttendanceStatus.PRESENT -> 1.0
+                AttendanceStatus.LATE -> 0.5
+                AttendanceStatus.ABSENT -> 0.0
+            }
+        }
+
+        val percentage = (points / TOTAL_SESSIONS) * 100
+
+        return Result.success(percentage)
+    }
+
+    companion object {
+        const val TOTAL_SESSIONS: Int = 3
+    }
 }
 
-}
