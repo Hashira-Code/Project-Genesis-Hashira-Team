a716301733db9483d134d71015e7448ed308b004
Mona Nada_hub <monaabonada409755535@gmail.com>
2026-05-09 15:26:09 +0300

    refactor(domain): Add find teams with no project coroutine support

--- src/main/kotlin/domain/usecase/FindTeamsWithNoProjectUseCase.kt
+++ src/main/kotlin/domain/usecase/FindTeamsWithNoProjectUseCase.kt
@@ -4,21 +4,30 @@ import domain.model.entity.Project
 import domain.model.entity.Team
 import domain.repository.ProjectRepo
 import domain.repository.TeamRepo
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 
 class FindTeamsWithNoProjectUseCase(
     private val teamRepo: TeamRepo,
     private val projectRepo: ProjectRepo
 ) {
-    operator fun invoke(): Result<List<Team>> {
-        val teams = teamRepo.getAll().getOrElse { return Result.failure(it) }
-        val projects = projectRepo.getAll().getOrElse { return Result.failure(it) }
+    suspend operator fun invoke(): Result<List<Team>> = coroutineScope {
+        val teamsDeferred = async { teamRepo.getAll() }
+        val projectsDeferred = async { projectRepo.getAll() }
 
-        if (teams.isEmpty()) return Result.success(emptyList())
+        val teams = teamsDeferred.await().getOrElse {
+            return@coroutineScope Result.failure(it)
+        }
+        val projects = projectsDeferred.await().getOrElse {
+            return@coroutineScope Result.failure(it)
+        }
+
+        if (teams.isEmpty()) return@coroutineScope Result.success(emptyList())
 
         val teamIdsWithProjects = extractTeamIdsWithProjects(projects)
         val result = filterTeamsWithoutProjects(teams, teamIdsWithProjects)
 
-        return Result.success(result)
+        Result.success(result)
     }
 
     private fun extractTeamIdsWithProjects(projects: List<Project>): Set<String> {
