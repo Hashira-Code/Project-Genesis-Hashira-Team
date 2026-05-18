fec59c71cc49754e43753db1fd1ba86bb2d291de
Mona Nada_hub <monaabonada409755535@gmail.com>
2026-05-18 12:52:37 +0300

    build: add Koin compiler plugin and annotations support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.20"
+    id("io.insert-koin.compiler.plugin") version "1.0.0-RC2"
 }
 
 group = "org.bytebloom"
@@ -14,13 +15,15 @@ dependencies {
     testImplementation("com.google.truth:truth:1.4.5")
     testImplementation("io.mockk:mockk:1.14.9")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
-    implementation("io.insert-koin:koin-core:4.1.1")
+    implementation("io.insert-koin:koin-core:4.2.0")
+    implementation("io.insert-koin:koin-annotations:4.2.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(24)
 }
